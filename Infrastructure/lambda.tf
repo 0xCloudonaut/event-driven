b4c52c291feefabe8070410bd33beccee7aec67a
0xCloudonaut <atharva.dandapur19@gmail.com>
2026-03-14 05:25:11 +0530

    Attached the AWSLambdaSQSQueueExecutionRole policy to the lambda role

--- Infrastructure/lambda.tf
+++ Infrastructure/lambda.tf
@@ -30,34 +30,14 @@ resource "aws_lambda_function" "order_analytics_event" {
   }
 }
 
-# create lambda function for processing order events
-resource "aws_lambda_function" "order_processing_event" {
-  function_name = "order_processing_event"
-  runtime      = "python3.8"
-  role        = aws_iam_role.lambda_exec.arn
-  handler     = "lambda_function.lambda_handler"
-
-  source_code_hash = filebase64sha256("lambda_function.zip")
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
+# create event source mapping for order_analytics_event
+resource "aws_lambda_event_source_mapping" "order_analytics_event" {
+  event_source_arn = aws_sqs_queue.order_analytics_queue.arn
+  function_name    = aws_lambda_function.order_analytics_event.function_name
+  enabled         = true
 }
 
-# creating lambda function policy for order_analytics_event to poll and delete the order_analytics_queue
-resource "aws_lambda_permission" "order_analytics_event" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.order_analytics_event.function_name
-  principal     = "sns.amazonaws.com"
-
-  # The ARN of the SNS topic
-  source_arn = aws_sns_topic.order_processing_topic.arn
-}
-
-# creating a role for the order_analytics_event
+# create AWSLambdaSQSQueueExecutionRole for order_analytics_event to poll and delete from order_analytics_queue
 resource "aws_iam_role" "order_analytics_event" {
   name = "order_analytics_event_role"
 
@@ -76,30 +56,36 @@ resource "aws_iam_role" "order_analytics_event" {
   })
 }
 
-# attaching the policy for order_analytics_event to the order_analytics_event role
+#attach policy AWSLambdaSQSQueueExecutionRole to the order_analytics_event role
 resource "aws_iam_role_policy_attachment" "order_analytics_event" {
-  policy_arn = aws_iam_policy.order_analytics_event.arn
+  policy_arn = aws_iam_policy.AWSLambdaSQSQueueExecutionRole.arn
   role       = aws_iam_role.order_analytics_event.name
 }
 
-# attaching the order_analytics_event role to the order_analytics_event lambda
-resource "aws_lambda_function_role" "order_analytics_event" {
-  function_name = aws_lambda_function.order_analytics_event.function_name
-  role         = aws_iam_role.order_analytics_event.arn
+# create lambda function for processing order events
+resource "aws_lambda_function" "order_processing_event" {
+  function_name = "order_processing_event"
+  runtime      = "python3.8"
+  role        = aws_iam_role.lambda_exec.arn
+  handler     = "lambda_function.lambda_handler"
+
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
 }
 
-# creating lambda function policy for order_processing_event to poll and delete the order_processing_queue
-resource "aws_lambda_permission" "order_processing_event" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.order_processing_event.function_name
-  principal     = "sns.amazonaws.com"
-
-  # The ARN of the SNS topic
-  source_arn = aws_sns_topic.order_processing_topic.arn
+# create event source mapping for order_processing_event
+resource "aws_lambda_event_source_mapping" "order_processing_event" {
+  event_source_arn = aws_sqs_queue.order_processing_queue.arn
+  function_name    = aws_lambda_function.order_processing_event.function_name
+  enabled         = true
 }
 
-# creating a role for the order_processing_event
+# create AWSLambdaSQSQueueExecutionRole for order_processing_event to poll and delete from order_processing_queue
 resource "aws_iam_role" "order_processing_event" {
   name = "order_processing_event_role"
 
@@ -118,14 +104,8 @@ resource "aws_iam_role" "order_processing_event" {
   })
 }
 
-# attaching the policy for order_processing_event to the order_processing_event role
+#attach policy AWSLambdaSQSQueueExecutionRole to the order_processing_event role
 resource "aws_iam_role_policy_attachment" "order_processing_event" {
-  policy_arn = aws_iam_policy.order_processing_event.arn
+  policy_arn = aws_iam_policy.AWSLambdaSQSQueueExecutionRole.arn
   role       = aws_iam_role.order_processing_event.name
-}
-
-# attaching the order_processing_event role to the order_processing_event lambda
-resource "aws_lambda_function_role" "order_processing_event" {
-  function_name = aws_lambda_function.order_processing_event.function_name
-  role         = aws_iam_role.order_processing_event.arn
 }
