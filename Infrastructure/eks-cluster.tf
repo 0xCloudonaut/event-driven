9ded5d6dadc4c0fa2cf92d1c7e175e8a6fcda6c5
0xCloudonaut <atharva.dandapur19@gmail.com>
2026-03-24 05:58:09 +0530

    Refactoring the cluster and fargate setup

--- Infrastructure/eks-cluster.tf
+++ Infrastructure/eks-cluster.tf
@@ -7,7 +7,7 @@ module "eks" {
   kubernetes_version = "1.33"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = concat(module.vpc.private_subnets, module.vpc.public_subnets)
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {}
 
@@ -22,36 +22,16 @@ module "fargate_profile" {
   source = "terraform-aws-modules/eks/aws//modules/fargate-profile"
 
   name         = "observability-fargate-profile"
-  cluster_name = local.cluster_name
+  cluster_name = module.eks.cluster_name
 
   subnet_ids = module.vpc.private_subnets
-  selectors = [{
-    namespace = "kube-system"
-  }]
+  selectors = [
+    {namespace = "kube-system"}, 
+    {namespace = "default"}
+  ]
 
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
-}
-
-# Creating IAM role for fargate profile
-resource "aws_iam_role" "fargate_pod_execution_role" {
-  name = "eks-fargate-pod-execution-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks-fargate-pods.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-# Attaching the role to the fargate profile
-resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy_attachment" {
-  role       = aws_iam_role.fargate_pod_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess"
 }
