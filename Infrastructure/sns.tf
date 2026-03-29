b1885dd138f85f34a5b7c075c4636e18914ad82e
0xCloudonaut <atharva.dandapur19@gmail.com>
2026-03-29 15:00:00 +0200

    Adding event source mapping in SNS for subscribed queues

--- Infrastructure/sns.tf
+++ Infrastructure/sns.tf
@@ -26,6 +26,12 @@ resource "aws_sns_topic_subscription" "inventory_management_queue_subscription" 
   endpoint  = aws_sqs_queue.inventory_management_main.arn
 }
 
+# Creating aws lambda event source mapping for inventory management function
+resource "aws_lambda_event_source_mapping" "inventory_management_event" {
+  event_source_arn = aws_sns_topic.order_place_topic.arn
+  function_name    = aws_lambda_function.inventory_management.arn
+}
+
 # Fan out the same order events to the analytics queue.
 resource "aws_sns_topic_subscription" "notification_main_queue_subscription" {
   topic_arn = aws_sns_topic.order_place_topic.arn
@@ -33,3 +39,8 @@ resource "aws_sns_topic_subscription" "notification_main_queue_subscription" {
   endpoint  = aws_sqs_queue.notification_main.arn
 }
 
+# Creating aws lambda event source mapping for notification function
+resource "aws_lambda_event_source_mapping" "notification_event" {
+  event_source_arn = aws_sns_topic.order_place_topic.arn
+  function_name    = aws_lambda_function.notification_lambda.arn
+}
